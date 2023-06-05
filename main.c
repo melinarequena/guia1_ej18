650a806d542260ff5403749f6994c8f80d3c9379
Melina <mrequena@fue.undef.edu.ar>
2023-06-05 16:18:07 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,9 @@
+/*Diseñe una función que permita insertar un valor, de forma ordenada, dentro de una
+lista de enteros.*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
